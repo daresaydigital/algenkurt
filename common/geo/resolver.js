7faf52689c39ce88ff6a1e960e157184761a380f
Martin Kurtsson <martin.kurtsson@screeninteraction.com>
2015-09-22 16:12:28 +0200

    Styling

---
+++ common/geo/resolver.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var React = require('react-native');
+var { Text } = React;
+
+class Resolver extends React.Component {
+  render() {
+    return (<Text>Current Position: XXX</Text>);
+  }
+
+}
+
+module.exports = Resolver;
--- common/main-view.js
+++ common/main-view.js
@@ -1,20 +1,26 @@
 'use strict';
 
 var React = require('react-native');
-
+var style = require('./style');
 var {
   Text,
   View
 } = React;
 
 class MainView extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+  }
+
   render() {
-    var str = 'Hej Erik!';
+    var header = 'Älgens väder';
+    var location = 'Unknown location';
+    var locationStyle = [style.p, style.error];
     return (
-      <View>
-        <Text>
-          {str}
-        </Text>
+      <View style={style.main_view}>
+        <Text style={style.h1}>{header}</Text>
+        <Text style={locationStyle}>{location}</Text>
       </View>
     );
   }
